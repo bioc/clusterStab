6d6f39a7746162e8428b4fbe6b80c656f715667c
James MacDonald <jmacdon@med.umich.edu>
2008-10-09 15:03:49 +0000

    Fixed warning that arose because I was overwriting generic 'show' method.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/clusterStab@34352 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/AllGenerics.R
+++ R/AllGenerics.R
@@ -1,6 +1,6 @@
 setGeneric("benhur", function(object, ...) standardGeneric("benhur"))
 setGeneric("clusterComp", function(object, ...) standardGeneric("clusterComp"))
-setGeneric("show", function(object) standardGeneric("show"))
+##setGeneric("show", function(object) standardGeneric("show"))
 setGeneric("hist", function(x, ...) standardGeneric("hist"))
 setGeneric("ecdf", function(x) standardGeneric("ecdf"))
 
